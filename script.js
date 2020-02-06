27bc7e5bc0c52bb3522f90384ed7b7638c3429f1
Joe Anton <janton153@gmail.com>
2020-02-06 15:26:00 -0800

    vectors and edge detection

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
+let p;
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+  p = new Cat();
 }
 
 function draw(){
-  if(mouseIsPressed){
-    fill(0);
-    circle(mouseX,mouseY,40);
+  background(55,100,144);
+  p.update();
+  p.draw();
+  p.edges();
+}
+
+class Cat {
+  constructor(){
+    //position - vector: entitity with direction and magnitude on xy axis
+    this.pos = createVector(random(width), random(height));
+    //velocity - left or right, up or down, and speed
+    this.vel = createVector(random(-2,2), random(-2,2));
+    ///size
+    this.size = 10;
   }
 
+//update movement by adding velocity
+  update(){
+    this.pos.add(this.vel);
+  }
+
+  draw() {
+    noStroke();
+    fill('rbga(255,255,255)');
+    circle(this.pos.x, this.pos.y, this.size)
+  }
+
+  //detect edges
+  edges() {
+    if(this.pos.x < 0 || this.pos.x > width){
+      this.vel.x *= -1;
+    }
+    if(this.pos.y < 0 || this.pos.y > height){
+      this.vel.y *= -1;
+    }
+  }
 }
