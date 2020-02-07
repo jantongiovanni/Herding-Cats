ecef14bacf41d16c1436b9093e06f97e0418e3a8
Joe Anton <janton153@gmail.com>
2020-02-06 16:06:08 -0800

    vectors connected with variable alpha stroke

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-let p;
+const cats = [];
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  p = new Cat();
+  //based on size of window
+  const catsNum = Math.floor(window.innerWidth/10);
+  //console.log(catsNum);
+  for(let i = 0; i < catsNum; i++){
+    cats.push(new Cat());
+  }
 }
 
 function draw(){
   background(55,100,144);
-  p.update();
-  p.draw();
-  p.edges();
+  cats.forEach((cat, index) => {
+    cat.update();
+    cat.drawCat();
+    cat.checkCats(cats.slice(index));
+  })
+
+
 }
 
 class Cat {
@@ -19,17 +28,18 @@ class Cat {
     //velocity - left or right, up or down, and speed
     this.vel = createVector(random(-2,2), random(-2,2));
     ///size
-    this.size = 10;
+    this.size = 5;
   }
 
 //update movement by adding velocity
   update(){
     this.pos.add(this.vel);
+    this.edges();
   }
 
-  draw() {
+  drawCat() {
     noStroke();
-    fill('rbga(255,255,255)');
+    fill('rbga(255,255,255, 0.5)');
     circle(this.pos.x, this.pos.y, this.size)
   }
 
@@ -42,4 +52,21 @@ class Cat {
       this.vel.y *= -1;
     }
   }
+
+  //connect
+  checkCats(cats){
+    let lineStroke;
+    let lineDist = 120;
+    cats.forEach(cat => {
+      const d = dist(this.pos.x, this.pos.y, cat.pos.x, cat.pos.y);
+      if(d < lineDist) {
+        lineStroke = color(255,255,255);
+        lineStroke.setAlpha((lineDist-d)*2);
+        stroke(lineStroke);
+        line(this.pos.x, this.pos.y, cat.pos.x, cat.pos.y);
+      }
+
+    })
+  }
+
 }
